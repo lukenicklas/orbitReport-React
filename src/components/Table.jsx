aff0606884506704a535f0705d2bc7dff032c904
lukenicklas <lukenicklas18@gmail.com>
2023-12-15 10:28:16 -0600

    On to Part 4

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,29 @@
+
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
+      {sat.map(({ id, data }) => (
+        <tr key={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational}</td>
      </tr>
+      ))}
      </tbody>
    </table>
   );
 };
+
 
 export default Table;

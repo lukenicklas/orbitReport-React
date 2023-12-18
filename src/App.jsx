505fb5a24d01b0373e8b5d75b49b6a42645ca27f
lukenicklas <lukenicklas18@gmail.com>
2023-12-18 17:19:45 -0600

    Final complete but need to enable github actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
-import { useState } from 'react'
+import { useState } from 'react' //this is a react hook that allows us to add state to a functional component. It returns an array with two values, the current state and a function to update it.
 import satData from './components/satData';
 
-function App() {
-  const [sat, setSat] = useState(satData);
- 
-  const filterByType = (currentType) => {
-    const filteredSats = satData.filter((newSatDisplay) => {
-       return newSatDisplay.orbitType === currentType;
+function App() { //creating the buttons
+  const [sat, setSat] = useState(satData); //using useState function to update the state
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))]; //using the map method to extract orbit types from each sat.
+  const filterByType = (currentType) => { //here we are defining a function that will filter the satellite data based on orbit type
+    const filteredSats = satData.filter((newSatDisplay) => { //uses filter method to create a new array that contains only the satellites that match the orbit type I want.
+      return newSatDisplay.orbitType === currentType; //checking to make sure the orbit type is the same as the currentType
     });
-    setSat(filteredSats);
-   };
-  
-  //const displaySats = [...new Set(satData.map((data) => data.orbitType))];
-  //const filterByType = (currentType) => {
-    //const displaySats = satData.filter((newSatDisplay) => {
-       //return newSatDisplay.orbitType === currentType;
-   
-    const displaySats = [...new Set(satData.map((data) => data.orbitType))];
- 
+    setSat(filteredSats); //updates the sat state with the filtered satellites.
+  };
+
+
   return (
     <div>
       <Banner />
-      <Buttons 
-          filterByType={filterByType}
-          setSat={setSat}
-          displaySats={displaySats}
-          />
-      <Table 
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table
         sat={sat}
       />
     </div>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,9 @@
 import satData from './satData';
 
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+const Buttons = ({ filterByType, setSat, displaySats }) => { //passing in the three props
   return (
     <div>
-      {displaySats.map((sat, id) => {
+      {displaySats.map((sat, id) => { //mapping over displaySats array and is creating a new array of buttons based on each orbit type
         return (
           <button onClick={() => filterByType(sat)} key={id}>
             {sat} Orbit
@@ -14,5 +14,5 @@ const Buttons = ({ filterByType, setSat, displaySats }) => {
     </div>
   );
 };
-
+//when the button is clicked  the filterbytype function is called with sat as the argument. the second onclick is calling setData as teh argument resetting the displayed sat orbits
 export default Buttons;
